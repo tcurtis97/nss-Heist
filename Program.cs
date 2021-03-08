cf769f5e5c1710c5f6b984f01841d3c39682f919
Tim Curtis <timcurtis97@yahoo.com>
2021-03-08 14:05:22 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Team.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace heist
+{
+    public class Teammate
+    {
+        public string Name { get; set; }
+        public int Skill { get; set; }
+        public decimal Courage { get; set; }
+    }
+
+
+
+
+
+}
